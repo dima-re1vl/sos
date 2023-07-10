69f21ef9c4165e34999176b4fb5a06abb89d4bcf
re1vl <re1vlmail@gmail.com>
2023-07-10 11:39:31 +0300

    added features section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+// ................................................................................
 // slider (what_is_section)
+// ................................................................................
 
 const imgCollection = document.querySelectorAll('.what-img');
 const arrow = document.querySelector('.what-right');
@@ -37,3 +39,33 @@ function addActive(x) {
     imgCollection[x].classList.add('active')
     circles[x].classList.add('active-circle')
 }
+
+// ................................................................................
+// slider info (features_section)
+// ................................................................................
+
+const blocks = document.querySelectorAll('.block');
+const infos = document.querySelectorAll('.info');
+const balls = document.querySelectorAll('.circle-features');
+let idxFeatures = 0;
+
+balls.forEach(item => {
+    item.addEventListener('click', e => {
+        deleteActiveFeatures();
+        idxFeatures = +e.target.dataset.value;
+        addActiveFeatures(idxFeatures);
+    })
+})
+
+function deleteActiveFeatures() {
+    blocks.forEach(x => x.classList.remove('active-title'));
+    infos.forEach(x => x.classList.remove('active-info'));
+    balls.forEach(x => x.classList.remove('active-features'));
+}
+
+function addActiveFeatures(idx) {
+    blocks[idx].classList.add('active-title');
+    infos[idx].classList.add('active-info');
+    balls[idx].classList.add('active-features');
+}
+
